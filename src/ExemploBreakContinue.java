b0682093fd3504ce8175700daeddedd6cc601179
Ana Joyce <anajoyce2066@outlook.com>
2023-10-19 08:39:00 -0300

    exercises

---
+++ src/ExemploBreakContinue.java
@@ -0,0 +1,13 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        for (int numero = 1; numero <= 5; numero++){
+            if (numero == 3 )
+
+            // o contador para no numero 3
+            break;
+            //continue; o contador pula o 3 mas continua a contagem ate o 5
+
+            System.out.println(numero);
+        }
+    }
+}
---
+++ src/ExemploForEach.java
@@ -0,0 +1,8 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos [] = {"ANA", "FABIO", "CHICO", "CARLOS"};
+        for (String aluno : alunos){
+            System.out.println("O nome do alune é: "  + aluno);
+        }
+    }
+}
